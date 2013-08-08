8632c0fa398248683e0e42de35741bfe60f6b82b
Riboloff <makcimgovorov@gmail.com>
2013-08-09 00:46:57 +0400

    first test: jenkins

---
+++ solution/test/test_jenkins.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+import unittest
+from nose.tools import eq_
+
+
+class TestSum(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_jenkins(self):
+        eq_(1, 0)
