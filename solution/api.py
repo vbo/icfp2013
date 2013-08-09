bce060309e4e730a3646944501ee5dcdb4d71cea
Borodin Vadim <borodin.vadim@gmail.com>
2013-08-09 13:15:10 +0400

    api.eval, api.guess, tests

--- solution/api.py
+++ solution/api.py
@@ -1,10 +1,17 @@
 import json
+from time import sleep
 import requests
+
 import config
 
 
+request_delay = 0
+
+
 class RequestError(BaseException):
-    pass
+    def __init__(self, code):
+        super(RequestError, self).__init__(code)
+        self.code = code
 
 
 def call(path, request):
@@ -13,6 +20,8 @@ def call(path, request):
         params={"auth": config.auth_token},
         data=json.dumps(request)
     )
+    if request_delay:
+        sleep(request_delay)
     if response.status_code != 200:
         raise RequestError(response.status_code)
     return response.json()
@@ -20,4 +29,13 @@ def call(path, request):
 
 def train(size=None, operators=None):
     result = call("train", {"size": size, "operators": operators})
+    return result
+
+
+def eval(id, program, arguments):
+    result = call("eval", {"id": id, "program": program, "arguments": map(str, arguments)})
+    return result
+
+def guess(id, program):
+    result = call("guess", {"id": id, "program": program})
     return result
