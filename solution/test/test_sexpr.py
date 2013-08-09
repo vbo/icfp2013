1050941671f7b228490bfdfbe1c8f79b66077aaf
Riboloff <makcimgovorov@gmail.com>
2013-08-09 17:38:35 +0400

    add new tests on sexpr._solve

--- solution/test/test_sexpr.py
+++ solution/test/test_sexpr.py
@@ -30,3 +30,15 @@ class SexprTestCase(unittest.TestCase):
             sexpr._solve("(lambda (x_45397) (fold x_45397 0 (lambda (x_45397 x_45398) "
                          "(if0 (shr1 (plus (plus (shr4 (shr16 (or (or (shl1 x_45398) 0) x_45397))) "
                           "(shl1 x_45397)) x_45397)) x_45397 x_45397))))", Int64(0x1122334455667788)), 0x0000000000000011)
+
+        self.assertEquals(sexpr._solve("(lambda (x_31742) (or (or (shr16 (or (shr16 (shr4 0)) " 
+                                        "(xor (and (if0 (xor (or 1 0) x_31742) 1 1) x_31742) x_31742))) 0) x_31742))", 
+                                        Int64(0xABCDEF1234567890)), 0xABCDEFDFFF567CD6)
+        self.assertEquals(sexpr._solve("(lambda (x_36979) (and (shr16 (plus x_36979 (if0 (shr4 (shl1 x_36979))"
+                                        " (shr4 0) (shr4 (shr1 (shr1 (not (if0 (plus (shr4 (xor 1 x_36979)) x_36979)"
+                                        " x_36979 x_36979)))))))) x_36979))", 
+                                        Int64(0xABCDEF1234567890)), 0x0000AD1234546880)
+        self.assertEquals(sexpr._solve("(lambda (x_36979) (and (shr16 (plus x_36979 (if0 (shr4 (shl1 x_36979))"
+                                        " (shr4 0) (shr4 (shr1 (shr1 (not (if0 (plus (shr4 (xor 1 x_36979)) x_36979)"
+                                        " x_36979 x_36979)))))))) x_36979))", 
+                                        Int64(0xFFFFFFFFFFFFFFFF)), 0x0000FFFFFFFFFFFF)
