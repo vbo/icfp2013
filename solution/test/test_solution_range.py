455472e4bc15df1c532ee0aa4d3dfc94cf5671ea
lenny <lennytmp@gmail.com>
2013-08-10 14:45:11 +0400

    test fixed

--- solution/test/test_solution_range.py
+++ solution/test/test_solution_range.py
@@ -10,32 +10,7 @@ class SolutionTestRange(unittest.TestCase):
 
     tree_index_root = './tree_index'
 
-    def _transformXsToIds(self, text):
-        idsToTransform = re.findall(r'x_[0-9]*', text)
-        uniqIds = []
-        if (len(idsToTransform) > 0):
-            for x in idsToTransform:
-                if x not in uniqIds:
-                    uniqIds.append(x)
-        for i in range(len(uniqIds)):
-            if (i == 0):
-                text = text.replace(uniqIds[i], 'id')
-            else:
-                text = text.replace(uniqIds[i], 'id' + str(i+1))
-        return text
 
-    def _test_range(self,size):
-        programs = []
-        index = build_formula_index.TreeTemplatesIndex(self.tree_index_root)
-        for data in index.generate_formulas(size):
-            programs.append(data["s"])
-        result = api.train(size)
-        program = result["challenge"]
-        program = self._transformXsToIds(program)
-        self.assertTrue(program in programs, '%s not found' % (program))
-
-    def test_range3(self):
-        self._test_range(3)
 
 
 # skip to not DDoS game server
--- solution/test/test_solver_with_fixture.py
+++ solution/test/test_solver_with_fixture.py
@@ -1,7 +1,15 @@
-import json
-import unittest
-from .. import solver
+import json, unittest, re, sys
+from .. import solver, api, config
 from ..tools import build_formula_index
+
+ex_train = config.example_train
+
+api.request_delay = 5
+
+class SolutionTestRange(unittest.TestCase):
+
+    tree_index_root = './tree_index'
+
 
 class SolverTestWithFixture(unittest.TestCase):
 
@@ -20,12 +28,9 @@ class SolverTestWithFixture(unittest.TestCase):
                     self.assertEquals(result, expected, "%s is not %s for %s" % (
                         result, expected, conf["request"]["challenge"]))
 
-    @unittest.skip("Generated program will never be equal to string from fixture")
-    # Root cause:  our ids are different from ids in fixture.
-    def test_FunctionExistance(self):
+    def test_program_existance(self):
         with open("./fixture/solver_fixture.jsons") as fixture:
             index = build_formula_index.TreeTemplatesIndex(self.tree_index_root)
-
             for line in fixture:
                 if not line.strip():
                     continue
@@ -35,11 +40,36 @@ class SolverTestWithFixture(unittest.TestCase):
                 if size > 10:
                     continue
                 print '.'
-
-
+                program = self._transformXsToIds(program)
                 combinations = index.generate_formulas(
                     size, allowed_ops=conf["request"]["operators"])
-
                 self.assertTrue(any(program == possible_program['s']
                                     for possible_program in combinations),
                                 '%s not found. Size: %s' % (program, size))
+
+    def _transformXsToIds(self, text):
+        idsToTransform = re.findall(r'x_[0-9]*', text)
+        uniqIds = []
+        if (len(idsToTransform) > 0):
+            for x in idsToTransform:
+                if x not in uniqIds:
+                    uniqIds.append(x)
+        for i in range(len(uniqIds)):
+            if (i == 0):
+                text = text.replace(uniqIds[i], 'id')
+            else:
+                text = text.replace(uniqIds[i], 'id' + str(i+1))
+        return text
+
+    def _test_range(self,size):
+        programs = []
+        index = build_formula_index.TreeTemplatesIndex(self.tree_index_root)
+        for data in index.generate_formulas(size):
+            programs.append(data["s"])
+        result = api.train(size)
+        program = result["challenge"]
+        program = self._transformXsToIds(program)
+        self.assertTrue(program in programs, '%s not found' % (program))
+
+    def test_range3(self):
+        self._test_range(3)
