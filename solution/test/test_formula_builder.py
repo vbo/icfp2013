3f6e2803d1feca5bc384e6e9af109fcb487905cb
Riboloff <makcimgovorov@gmail.com>
2013-08-10 18:03:32 +0400

    add tests formula_builder

--- solution/test/test_formula_builder.py
+++ solution/test/test_formula_builder.py
@@ -7,12 +7,14 @@ class FormulaBuilderTestCase(unittest.TestCase):
     tree_index_root = './tree_index'
 
     formulas = [
-        ('\(lambda \(id\) \(not \(not 1\)\)\)', 4),
-        ('\(lambda \(id\) \(plus id 1\)\)', 4),
-        ('\(lambda \(id\) \(if0 .*and', 7),
-        ('\(lambda \(id\) \(if0 .*or', 7),
-        ('\(lambda \(id\) \(if0 .*xor', 7),
-        ('\(lambda \(id\) \(fold', 7),
+        ('\(lambda \(id\) \(not \(not 1\)\)\)', 4, None),
+        ('\(lambda \(id\) \(plus id 1\)\)', 4, None),
+        ('\(lambda \(id\) \(if0 .*and', 7, None),
+        ('\(lambda \(id\) \(if0 .*or', 7, None),
+        ('\(lambda \(id\) \(if0 .*xor', 7, None),
+        ('\(lambda \(id\) \(fold', 7, None),
+        ('\(lambda \(id\) \(fold id 0 \(lambda \(id\d id\d\) \(not \(not id\)\)\)\)\)', 8, ['tfold', 'not']),
+        ('\(lambda \(id\) \(fold', 9, ['tfold', 'if0']),
     ]
 
     @classmethod
@@ -20,15 +22,14 @@ class FormulaBuilderTestCase(unittest.TestCase):
         cls.index = build_formula_index.TreeTemplatesIndex(cls.tree_index_root)
         cls.formulas_cache = {}
 
-    def _get_cached_formulas(self, size):
+    def _get_cached_formulas(self, size, operators):
         if size not in self.formulas_cache:
-            self.formulas_cache[size] = list(self.index.generate_formulas(size))
+            self.formulas_cache[size] = list(self.index.generate_formulas(size, operators))
 
         return self.formulas_cache[size]
 
-    def _check_formula_is_present(self, formula_regexp_text, size):
-        formulas = self._get_cached_formulas(size)
-
+    def _check_formula_is_present(self, formula_regexp_text, size, operators):
+        formulas = self._get_cached_formulas(size, operators)
         formula_regexp = re.compile(formula_regexp_text)
         equals = 0
 
@@ -40,5 +41,5 @@ class FormulaBuilderTestCase(unittest.TestCase):
         self.assertEqual(equals, 1, "Formula not found: '%s'" % formula_regexp_text)
 
     def test_formulas(self):
-        for f_regexp, size in self.formulas:
-            self._check_formula_is_present(f_regexp, size)
+        for f_regexp, size, operators in self.formulas:
+            self._check_formula_is_present(f_regexp, size, operators)
