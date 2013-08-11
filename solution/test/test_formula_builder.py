e59881eacae271c3723957f6714cd6eb7f9d6739
Riboloff <makcimgovorov@gmail.com>
2013-08-11 04:25:52 +0400

    fixed tests formula_builder

--- solution/test/test_formula_builder.py
+++ solution/test/test_formula_builder.py
@@ -8,15 +8,15 @@ class FormulaBuilderTestCase(unittest.TestCase):
     tree_index_root = './tree_index'
 
     formulas = [
-       # ('\(lambda \(id\) \(not \(not 1\)\)\)', 4, None, 1),
-       # ('\(lambda \(id\) \(plus id 1\)\)', 4, None, 2),
+        ('\(lambda \(id\) \(not \(not 1\)\)\)', 4, None, 1),
+        ('\(lambda \(id\) \(plus id 1\)\)', 4, None, 2),
         ('\(lambda \(id\) \(if0 .*and', 7, None, 3),
         ('\(lambda \(id\) \(if0 .*or', 7, None, 4),
         ('\(lambda \(id\) \(if0 .*xor', 7, None, 5),
         ('\(lambda \(id\) \(fold', 7, None, 6),
         ('\(lambda \(id\) \(fold id 0 \(lambda \(id\d id\d\) \(not \(not id\)\)\)\)\)', 8, ['tfold', 'not'], 7),
         ('\(lambda \(id\) \(fold', 9, ['tfold', 'if0'], 8),
-        ('\(lambda \(id\) \(xor id \(or 0 \(shr4 \(shr16 id\)\)\)\)\)', 8, ['xor', 'or', 'shr4', 'shr16'], 9),
+        ('\(lambda \(id\) \(xor id \(shr4 \(shr16 id\)\)\)\)', 8, ['xor', 'or', 'shr4', 'shr16'], 9),
         ('\(lambda \(id\) \(fold id 0 \(lambda \(id2 id3\) \(not \(plus id2 id3\)\)\)\)\)', 9, ['tfold', 'plus', 'not'], 11),
         ('\(lambda \(id\) \(fold id 0 \(lambda \(id2 id3\) \(shl1 \(xor 1 id2\)\)\)\)\)', 9, ['shl1', 'tfold', 'xor'], 12),
         ('\(lambda \(id\) \(fold id 0 \(lambda \(id2 id3\) \(if0 id2 id2 id2\)\)\)\)', 9, ['tfold', 'if0'], 13),
@@ -43,10 +43,10 @@ class FormulaBuilderTestCase(unittest.TestCase):
        #    print formulas
 
         for f in formulas:
-            if (number == 3):
-               print f['s']
+          #  if (number == 9):
+          #     print f['s']
             if formula_regexp.match(f['s']):
-               equals = 0
+               equals = 1
                break
 
         self.assertEqual(equals, 1, "Test number: " + str(number) + "\n\t\tFormula not found: '%s'" % formula_regexp_text)
