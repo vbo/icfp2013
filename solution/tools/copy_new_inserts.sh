f3817e64d09cd2d7601439c2f46f618f7fc7d88b
Denis Yakunin <de.yakunin@gmail.com>
2013-08-11 20:06:27 +0400

    copy new inserts

--- solution/tools/copy_new_inserts.sh
+++ solution/tools/copy_new_inserts.sh
@@ -15,9 +15,9 @@ while : ; do
         ((NUM+=1))
     done
 
-    comm -23 $1 $2 2>/dev/null | tee >> $2 $TARGET_FILE
+    diff --unchanged-line-format= --old-line-format= --new-line-format='%L' $2 $1 2>/dev/null | tee >> $2 $TARGET_FILE
 
-    head -n $(( `wc -l < $TARGET_FILE` - 1 )) $TARGET_FILE
+    head -n $(( `wc -l 2>/dev/null < $TARGET_FILE` - 1 )) $TARGET_FILE 1>/dev/null
 
     cp $TARGET_FILE ~/Dropbox/Icfp2013/problems_index/
 
