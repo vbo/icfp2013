f5504c4d0438dc4db80adc941ccf024fa5e531d1
Nikolay Zakharov <nikolay@desh.su>
2013-08-10 23:10:17 -0700

    API: Sleep before sending request and only for time that is left to wait.

--- solution/api.py
+++ solution/api.py
@@ -1,5 +1,5 @@
 import json
-from time import sleep
+import time
 import requests
 from requests.exceptions import Timeout
 
@@ -10,6 +10,7 @@ request_delay = 0
 default_call_url = call_url = lambda x: str(config.service_url) + x
 default_timeout = timeout = 10
 default_auto_retry = auto_retry = True
+last_api_request_time = 0
 
 
 class RequestError(BaseException):
@@ -23,15 +24,23 @@ class AlreadySolvedException(BaseException):
 
 
 def call(path, request):
+    global last_api_request_time
+
+    elapsed_since_last_call = time.time() - last_api_request_time
+    if elapsed_since_last_call < request_delay:
+        time.sleep(request_delay - elapsed_since_last_call)
+
     try:
+        # TODO: consider getting last api time AFTER the call (need to do that
+        # in exception handler if sleeping is desired for auto_retry
+        last_api_request_time = time.time()
         response = requests.post(
             call_url(path),
             params={"auth": config.auth_token},
             data=json.dumps(request),
             timeout=10
         )
-        if request_delay:
-            sleep(request_delay)
+
         if response.status_code == 412:
             raise AlreadySolvedException()
         if response.status_code != 200:
