69736f6e1342d77b33c59338f97a6343b7b3f683
Nikolay Zakharov <nikolay@desh.su>
2013-08-10 21:48:59 -0700

    Simplify broken template test

--- solution/test/test_template_render.py
+++ solution/test/test_template_render.py
@@ -7,10 +7,10 @@ class TemplateRenderTestCase(unittest.TestCase):
 
     @attr('fast')
     def test_fold_variables_without_fold(self):
-        template_proto = ('op1', ('op1', ('if0', 'T', ('fold', 'T', 'T', 'T'), 'T')))
+        template_proto = ('if0', 'T', ('fold', 'T', 'T', 'T'), 'T')
         template = TemplatedProgramTreeNode.fromtuple(template_proto)
 
-        for formula in template.render(False):
+        for i, formula in enumerate(template.render(False)):
             s = formula_to_string(formula)
             if ('id2' in s or 'id3' in s):
-                self.assertTrue('fold' in s, 'Has folded ids BUT NO FOLD: "%s"' % formula)
+                self.assertTrue('fold' in s, '[%d] Has folded ids BUT NO FOLD: "%s"' % (i, formula))
